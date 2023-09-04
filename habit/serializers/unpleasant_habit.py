e046edffe744de49ee23a0efd8443841f1a21b98
Denis <isxakov.den@bk.ru>
2023-09-04 11:12:30 +0300

    Updated serializer

--- habit/serializers/unpleasant_habit.py
+++ habit/serializers/unpleasant_habit.py
@@ -1,11 +1,10 @@
 from rest_framework import serializers
 
-from habit.models import UnpleasantHabit
+from habit.models import UnpleasantHabit, PleasantHabit
 from habit.validators import validate_habit_time
 
 
 class UnpleasantHabitSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = UnpleasantHabit
         fields = '__all__'
@@ -14,7 +13,19 @@ class UnpleasantHabitSerializer(serializers.ModelSerializer):
 class UnpleasantHabitCreateSerializer(serializers.ModelSerializer):
     duration = serializers.IntegerField(validators=[validate_habit_time])
 
-    # def
+    def validate(self, attrs):
+        if 'pleasant_habit' in attrs:
+            pleasant_habit_id = attrs.get('pleasant_habit').id
+            pleasant_habit = PleasantHabit.objects.filter(id=pleasant_habit_id).first()
+            if pleasant_habit.is_pleasant_habit is False:
+                raise serializers.ValidationError(
+                    "К связанным привычкам можно отнести только привычки с признаком приятной привычки.")
+
+        if attrs.get('pleasant_habit') is None and attrs.get('reward') is None:
+            raise serializers.ValidationError("Вы должны выбрать соответствующую привычку и вознаграждение ")
+
+        if attrs.get('pleasant_habit') and attrs.get('reward'):
+            raise serializers.ValidationError("Нельзя одновременно выбрать связанную привычку и вознаграждение")
 
     class Meta:
         model = UnpleasantHabit
